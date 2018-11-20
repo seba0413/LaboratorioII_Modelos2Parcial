51a2e0742cea95d4370b3cbc57ad1884c752e2a4
Sebastian Pasquini <32931668+seba0413@users.noreply.github.com>
2018-11-20 10:34:06 -0300

    Proyecto Votacion

---
+++ Votacion/Entidades/Dao.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+
+namespace Entidades
+{
+    public class Dao : IArchivos<Votacion>
+    {
+        public bool Guardar(string rutaArchivo, Votacion objeto)
+        {
+            bool todoOk = false;
+            String connectionStr = "Data Source=DESKTOP-PFDR43B;Initial Catalog=votacion-sp-2018.bak;Integrated Security=True";
+            SqlConnection conexion = null;
+            SqlCommand comando; 
+
+            try
+            {
+                conexion = new SqlConnection(connectionStr);
+                comando = new SqlCommand();
+                comando.CommandType = CommandType.Text;
+                comando.Connection = conexion;
+
+                string consulta = String.Format("INSERT INTO Votaciones (nombreLey, afirmativos, negativos, abstenciones) VALUES ('{0}', {1}, {2}, {3})", objeto.NombreLey, objeto.ContadorAfirmativo, objeto.ContadorNegativo, objeto.ContadorAbstencion);
+                comando.CommandText = consulta;
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                todoOk = true; 
+
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+            return todoOk;
+        }
+
+        public Votacion Leer(string rutaArchivo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Votacion/Entidades/SerializarXML.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+using Excepciones;
+
+namespace Entidades
+{
+    public class SerializarXML<T> : IArchivos<T>
+    {
+        public bool Guardar(string rutaArchivo, T objeto)
+        {
+            bool todoOk = false; 
+            XmlTextWriter writer = null;
+            XmlSerializer ser;
+
+            try
+            {
+                writer = new XmlTextWriter(rutaArchivo, Encoding.ASCII);
+                ser = new XmlSerializer(typeof(T));
+                ser.Serialize(writer, objeto);
+                todoOk = true;
+            }
+            catch (Exception)
+            {
+                throw new ErrorArchivoException("Error al serializar el objeto");
+            }
+            finally
+            {
+                writer.Close();
+            }
+            return todoOk;
+        }
+
+        public T Leer(string rutaArchivo)
+        {
+            T objeto;
+            XmlTextReader reader = null;
+            XmlSerializer ser;
+
+            try
+            {
+                reader = new XmlTextReader(rutaArchivo);
+                ser = new XmlSerializer(typeof(T));
+                objeto = (T)ser.Deserialize(reader);
+            }
+            catch (Exception)
+            {
+                throw new ErrorArchivoException("Error al deserializar el archivo");
+            }
+            finally
+            {
+                reader.Close();
+            }
+            return objeto;
+        }
+    }
+}
